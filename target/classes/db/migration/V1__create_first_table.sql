f080f4b9c85a451fbf611f7e3e1b2fea3e2d9007
chukalovai <topki4uk@mail.ru>
2026-04-09 08:55:46 +0300

    update .gitignore

--- target/classes/db/migration/V1__create_first_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS data_records (
-      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-      type INTEGER NOT NULL,
-      random_text VARCHAR(1000) NOT NULL,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
